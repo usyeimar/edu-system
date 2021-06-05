1b08045c035227929d82a86acaa2c6273f7bbfa5
yeimar <yeimar112003@outlook.com>
2021-06-04 23:18:01 -0500

    Error de accesos solucionado

--- App/ProcesarLogin.php
+++ App/ProcesarLogin.php
@@ -9,12 +9,11 @@ if (isset($_POST['btnLogin'])) {
 
     $ValidarUsuarios = new ValidarUsuarios($user,$pass);
     $UserArray = mysqli_fetch_array($ValidarUsuarios->LoginUsuario());
-    $ValidarUsuarios->CrearSessionId();
+    //$ValidarUsuarios->CrearSessionId();
    
     if($UserArray['UserExist'] > 0){
         $_SESSION['usuario'] = $user;
         $_SESSION['idUsuario'] = $idUsuario;
-        echo("alert($idUsuario;)");
         header("location:./admin/AdminPanel.php");
     }else{
         echo"DATOS INCORRECTOS";
