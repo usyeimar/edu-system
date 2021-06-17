946fef8cf4c4db2175dc7799a36efe393081745c
yeimar <yeimar112003@outlook.com>
2021-06-16 23:22:14 -0500

    edit usuario modal2

--- App/admin/Perfil.php
+++ App/admin/Perfil.php
@@ -75,7 +75,7 @@ require_once("./template/HeaderAdmin.php");
                                                 <div class="col-md-9">
                                                     <div class="tab-content">
                                                         <div class="tab-pane active" id="user-timeline">
-                                                            <h4>DATOS PERSONALES <a href="#edit-usuario"><button class="btn btn-sm btn-info" data-toggle="modal"  type="button"><i class="fas fa-pencil-alt" aria-hidden="true"></i></button></a></h4>
+                                                            <h4>DATOS PERSONALES<button class="btn btn-sm btn-info" data-toggle="modal" data-target="#edit-usuario" type="button"><i class="fas fa-pencil-alt" aria-hidden="true"></i></button></h4>
                                                             <div class="shadow p-3 mb-2 bg-body rounded">
                                                                 <table class="table table-bordered">
                                                                     <tbody>
